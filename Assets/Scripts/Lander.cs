4da0fac8d1faa2e26579c41e1afbf8b4de53c6ff
Hoodad14 <90596014+hoodadf@users.noreply.github.com>
2025-12-19 13:39:53 +0330

    added a simple movement system with force and torque from rigid body to have a smooth physic in movement

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -1,15 +1,30 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Lander : MonoBehaviour
-{
+public class Lander : MonoBehaviour {
+    private Rigidbody2D landerRigidbody2D;
+    private float forwardForce = 700f;
+    private float turnSpeed = 100f;
+    private void Awake() {
+        landerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+        if (Keyboard.current.upArrowKey.isPressed || Keyboard.current.wKey.isPressed) {
+            landerRigidbody2D.AddForce(forwardForce * transform.up * Time.deltaTime);
+        }
+        
+        if (Keyboard.current.upArrowKey.isPressed || Keyboard.current.dKey.isPressed) {
+            landerRigidbody2D.AddTorque(-turnSpeed * Time.deltaTime);
+        }
+        
+        if (Keyboard.current.upArrowKey.isPressed || Keyboard.current.aKey.isPressed) {
+            landerRigidbody2D.AddTorque(+turnSpeed * Time.deltaTime);        
+        }
+    }
 
     private void Update() {
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            Debug.Log("up(old)");
-        }
-        if (Keyboard.current.upArrowKey.isPressed) {
-            Debug.Log("up(new)");
-        }
+        
     }
 }
